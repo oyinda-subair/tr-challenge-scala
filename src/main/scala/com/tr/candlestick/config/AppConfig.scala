f7b1d9b5544a6b8314388fcb2448654522a1bfdf
oyinda-subair <oyinda.subair@gmail.com>
2022-05-16 01:43:05 +0100

    add collection types

--- src/main/scala/com/tr/candlestick/config/AppConfig.scala
+++ src/main/scala/com/tr/candlestick/config/AppConfig.scala
@@ -1,6 +1,10 @@
 package com.tr.candlestick.config
 
+import org.mongodb.scala.{Document, MongoCollection}
+
 object AppConfig {
   type ISIN = String
   type Price = Double
+  type InstrumentCollection = MongoCollection[Document]
+  type QuoteCollection = MongoCollection[Document]
 }
